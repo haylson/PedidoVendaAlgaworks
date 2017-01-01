ef583126ba43444c04dc8c4951be1f117d950a99
Haylson Martins <haylson@gmail.com>
2017-01-01 19:21:23 -0300

    commit inicial

---
+++ docs/querys.sql
@@ -0,0 +1,13 @@
+INSERT INTO usuario (id, email, nome, senha) VALUES (1,'admin@gmail.com','admin','admin');
+INSERT INTO usuario (id, email, nome, senha) VALUES (2,'vendedor@gmail.com','vendedor','vendedor');
+INSERT INTO usuario (id, email, nome, senha) VALUES (3,'auxiliar@gmail.com','auxiliar','auxiliar');
+
+INSERT INTO grupo(id, descricao, nome) VALUES (1,'ADMINISTRADORES','ADMINISTRADORES');
+INSERT INTO grupo(id, descricao, nome) VALUES (2,'VENDEDORES','VENDEDORES');
+INSERT INTO grupo(id, descricao, nome) VALUES (3,'AUXILIARES','AUXILIARES');
+
+INSERT INTO usuario_grupo(usuario_id, grupo_id) VALUES (1,1);
+INSERT INTO usuario_grupo(usuario_id, grupo_id) VALUES (2,2);
+INSERT INTO usuario_grupo(usuario_id, grupo_id) VALUES (3,3);
+
+INSERT INTO cliente(id, doc_receita_federal, email, nome, tipo) VALUES (1,'67251422816','cliente@gmail.com','CLIENTE 01','FISICA');
